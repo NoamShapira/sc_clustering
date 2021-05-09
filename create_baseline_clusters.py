42c1a116b8525b5568332eacf7cd61282812ad3d
NoamShapira <noam.shapi@gmail.com>
2021-05-09 22:32:24 +0300

    some typos fix, data loading is working

--- create_baseline_clusters.py
+++ create_baseline_clusters.py
@@ -1,11 +1,9 @@
 import os
 from pathlib import Path
 
-
 import config
 import utils
-from clustering.scanpy_cluster import create_clusters
-from data.data_loading import get_only_untreated_experints_in_one_anndata
+from data.data_loading import get_all_experiments_in_one_anndata
 
 
 def create_experiment_dir_and_return_path(experiment_name, main_results_dir: Path = config.RESULTS_DIR) -> Path:
@@ -18,6 +16,6 @@ def create_experiment_dir_and_return_path(experiment_name, main_results_dir: Pat
 if __name__ == '__main__':
     experiment_results_dir_path = create_experiment_dir_and_return_path("simple_clustering")
 
-    anndata = get_only_untreated_experints_in_one_anndata()
-    create_clusters(anndata, experiment_results_dir_path)
-    save_anndata_disk(experiment_results_dir_path)
+    adata = get_all_experiments_in_one_anndata()
+    # create_clusters(adata, experiment_results_dir_path)
+    adata.write(experiment_results_dir_path)
--- data/data_loading.py
+++ data/data_loading.py
@@ -13,17 +13,17 @@ def drop_treated_batches(df: pd.DataFrame) -> pd.DataFrame:
     raise NotImplemented
 
 
-def get_all_experiments_in_one_anndata(experimts_dir: Path = config.UMI_PATH,
+def get_all_experiments_in_one_anndata(experiments_data_dir: Path = config.UMI_PATH,
                                        meta_data_path: Path = config.META_DATA_PATH) -> ad.AnnData:
-    return get_experiments_in_one_anndata(experimts_dir, meta_data_path, [])
+    return get_experiments_in_one_anndata(experiments_data_dir, meta_data_path, [])
 
 
-def get_only_untreated_experints_in_one_anndata(experimts_dir: Path = config.UMI_PATH,
-                                                meta_data_path: Path = config.META_DATA_PATH) -> ad.AnnData:
-    return get_experiments_in_one_anndata(experimts_dir, meta_data_path, [drop_treated_batches])
+def get_only_untreated_experiments_in_one_anndata(experiments_data_dir: Path = config.UMI_PATH,
+                                                  meta_data_path: Path = config.META_DATA_PATH) -> ad.AnnData:
+    return get_experiments_in_one_anndata(experiments_data_dir, meta_data_path, [drop_treated_batches])
 
 
-def get_experiments_in_one_anndata(experimts_dir: Path, meta_data_path: Path,
+def get_experiments_in_one_anndata(experiments_data_dir: Path, meta_data_path: Path,
                                    batch_filter_functions: List[Callable[[pd.DataFrame], pd.DataFrame]]) -> ad.AnnData:
     # Read annotation file
     metadata = pd.read_table(meta_data_path)
@@ -31,13 +31,13 @@ def get_experiments_in_one_anndata(experimts_dir: Path, meta_data_path: Path,
         metadata = filter_func(metadata)
 
     # Read all plates into anndata and merge them
-    colnames = metadata.columns
+    col_names = metadata.columns
     adatas = []
     for index, row in metadata.iterrows():
-        cur_data = sc.read_text(str(experimts_dir) + row[meta_data_columns_names.BATCH_ID] + ".txt")
+        cur_data = sc.read_text(Path(experiments_data_dir, row[meta_data_columns_names.BATCH_ID] + ".txt"))
         cur_data = cur_data.T
-        for cname in colnames:
-            cur_data.obs[cname] = row[cname]
+        for col_name in col_names:
+            cur_data.obs[col_name] = row[col_name]
         adatas.append(cur_data)
         print(f"Reading , batch id - {row[meta_data_columns_names.BATCH_ID]}, "
               f"batch number - {index}, batch size - {metadata.shape[0]}")
