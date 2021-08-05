23086b69340961177fda99197fb19498f019bf8a
NoamShapira <noam.shapi@gmail.com>
2021-08-05 18:06:19 +0300

    adding more test for confution matrix

--- tests/data_proccecing_tests.py
+++ tests/data_proccecing_tests.py
@@ -12,4 +12,28 @@ def test_compute_confusion_matrix():
 
     assert set(cm.index) == {2, 3}
     assert set(cm.columns) == {1, 0}
-    assert (cm.values == np.array([[0., 1.], [1., 1.]])).all()
+
+    assert cm[1][2] == 1
+    assert cm[0][2] == 0
+    assert cm[1][3] == 1
+    assert cm[0][3] == 1
+
+
+def test_compute_confusion_matrix_more_than_2_classes_eye():
+    s1 = pd.Series(["0", "1", "2"], index=["a", "b", "c"])
+    s2 = pd.Series(["0", "1", "2"], index=["a", "b", "c"])
+
+    cm = compute_confusion_matrix(s1, s2)
+
+    assert (cm.values == np.eye(3)).all()
+
+
+def test_compute_confusion_matrix_more_than_2_classes():
+    s1 = pd.Series(["0", "1", "2"], index=["b", "a", "c"])
+    s2 = pd.Series(["0", "1", "2"], index=["a", "b", "c"])
+
+    cm = compute_confusion_matrix(s1, s2)
+    expected_result = np.eye(3)
+    expected_result[[0, 1]] = expected_result[[1, 0]]
+
+    assert (cm.values == expected_result).all()
