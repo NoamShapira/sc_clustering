7c0ce9514b9471ab6f1082683869bfa1402073b1
NoamShapira <noam.shapi@gmail.com>
2021-12-25 10:53:33 +0200

    run another train

--- embedding/load_hierarchically_trained_scvi.py
+++ embedding/load_hierarchically_trained_scvi.py
@@ -9,11 +9,10 @@ from embedding.embedding_config import DEFAULT_EMB_COL_NAME
 
 
 def load_adatas_from_experiment(experiment_name: str, result_dir: Path = config.RESULTS_DIR) -> \
-        Tuple[ad.AnnData, ad.AnnData, Dict[str, ad.AnnData], Dict[str, str]]:
+        Tuple[ad.AnnData, Dict[str, ad.AnnData], Dict[str, str]]:
     experiment_dir = Path(result_dir, experiment_name)
     full_adata_file_name = f"adata_with_embedding_in_{DEFAULT_EMB_COL_NAME}.h5ad"
-    adata_with_scanpy = ad.read_h5ad(Path(experiment_dir, "scanpy_metacells.h5ad"))
-    full_adata = ad.read_h5ad(Path(experiment_dir, full_adata_file_name))
+    full_adata_with_emb = ad.read_h5ad(Path(experiment_dir, full_adata_file_name))
     clusters_adatas = {}
     emb_coll_names = {"full": DEFAULT_EMB_COL_NAME}
     for root, subdirectories, _ in os.walk(experiment_dir):
@@ -21,6 +20,7 @@ def load_adatas_from_experiment(experiment_name: str, result_dir: Path = config.
             for file in [f for f in os.listdir(Path(root, sub_dir)) if ".h5ad" in f]:
                 clusters_adatas[sub_dir] = ad.read_h5ad(Path(root, sub_dir, file))
                 emb_coll_names[sub_dir] = file.split(".")[0].split("embedding_in_")[1]
-    return full_adata, adata_with_scanpy, clusters_adatas, emb_coll_names
+    return full_adata_with_emb, clusters_adatas, emb_coll_names
 
-# full_adata, adata_with_scanpy, clusters_adatas, emb_coll_names = load_adatas_from_experiment('hierarchical_scvi_2021_11_18__10_10_08')
+# example
+# full_adata_with_emb, clusters_adatas, emb_coll_names = load_adatas_from_experiment('hierarchical_scvi_2021_11_18__10_10_08')
--- scripts/train_scvi_hierarchically.py
+++ scripts/train_scvi_hierarchically.py
@@ -19,7 +19,7 @@ from utils import get_now_timestemp_as_string
 # this script assumes adata with raw count
 # It will normalize the counts and choose most variable gene (according to arguments)
 # It will not drop bad cells or bad genes (other that the highly variable genes)
-asaf_raw_adata_path = Path(config.ASAF_META_CELL_ATLAS_DIR_PATH, "cells.h5ad")
+asaf_raw_adata_path = Path(config.ASAF_META_CELL_ATLAS_DIR_PATH, "cells.h5ad") # /home/labs/amit/kenx/Serono/S15/cells.h5ad
 
 experiment_name = f"hierarchical_scvi_{get_now_timestemp_as_string()}"
 experiment_results_dir_path = Path(config.RESULTS_DIR, experiment_name)
@@ -37,13 +37,13 @@ parser.add_argument("--min_percentage_of_cluster_to_compute_new_embedding", type
 parser.add_argument("--split_and_continue_from_dir", type=str, default="")
 
 # preprocessing
-parser.add_argument("--pp_n_top_variable_genes", type=int, default=3000)
+parser.add_argument("--pp_n_top_variable_genes", type=int, default=8000)
 
 # scvi params
 parser.add_argument("--n_layers", type=int, default=1)
-parser.add_argument("--n_latent", type=int, default=20)
+parser.add_argument("--n_latent", type=int, default=16)
 parser.add_argument("--dropout_rate", type=float, default=0.5)
-parser.add_argument("--n_hidden", type=int, default=64)
+parser.add_argument("--n_hidden", type=int, default=128)
 
 args = parser.parse_args()
 
@@ -77,8 +77,8 @@ logging.info("spliting adata to major clusters")
 adatas_of_major_cluster = split_to_major_cluster(adata_with_scvi_emb, emb_col_name=emb_col_name,
                                                  clustering_model_name=args.clustering_method,
                                                  num_clusters=args.num_clusters_to_split_to)
-adata_with_scvi_emb.write(source_adata_path)
 
+logging.info("training scvi for the clusters")
 for i, adata_of_cluster in enumerate(adatas_of_major_cluster):
     split_results_dir_path = Path(args.save_results_to, f"cluster_{i}")
     os.mkdir(split_results_dir_path)
@@ -94,4 +94,4 @@ for i, adata_of_cluster in enumerate(adatas_of_major_cluster):
         logging.info(logging_msg)
         print(logging_msg)
 
-print("finished script !!")
+logging.info("finished script !!")
